562937f26fd09289d212305cc93a1c2e92f7c7f4
mohamedGaber2004 <mg7357432@gmail.com>
2026-01-03 01:34:15 +0200

    Done

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -11,13 +11,38 @@ from langgraph.prebuilt import ToolNode , tools_condition
 
 class GraphBuilder ():
     def __init__(self) : 
-        pass
+        self.tools = [
+            WeatherInfoTool(),
+            PlaceSearchTool(),
+            CalculatorTool(),
+            CurrencyCnverterTool()
+        ]
+        self.System_prompt = System_prompt
+        self.llm_with_tools = ModelLoader().load_llm()
 
-    def agent_function(self):
-        pass
+    def agent_function(self,state:MessageState):
+        """Main Agent Function"""
+        user_question = state["messages"]
+        input_question = [self.System_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages":[response]}
+
 
     def build_graph(self):
-        pass
+        graph_builder = StateGraph(MessageState) 
+
+        graph_builder.add_node("agent",self.agent_function)
+        graph_builder.add_node("tools",ToolNode(tools_condition))
+
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_conditional_edges("agent",tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent",END)
+
+        self.graph = graph_builder.compile()
+        return self.graph
+
+
 
     def __call__(self):
         pass
