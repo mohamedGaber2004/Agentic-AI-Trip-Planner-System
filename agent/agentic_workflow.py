7674a941ab85aaa4cbec2412620d4939271e1060
mohamedGaber2004 <mg7357432@gmail.com>
2026-01-03 18:55:24 +0200

    changes done

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -9,50 +9,46 @@ from tools.currency_conversions import CurrencyCnverterTool
 from langgraph.graph import START , END , StateGraph , MessageState
 from langgraph.prebuilt import ToolNode , tools_condition
 
-class GraphBuilder ():
-    def __init__(self,model_provider:str = "groq") : 
-        self.tools = []
-        self.weather_tools = WeatherInfoTool()
-        self.place_search_tool = PlaceSearchTool()
-        self.calculator_tool = CalculatorTool()
-        self.currency_converter_tool = CurrencyCnverterTool()
-
-        self.tools.extend([
-            * self.weather_tools.weather_tool_list,
-            * self.place_search_tool.place_search_tool_list,
-            * self.currency_converter_tool.currency_converter_tool_list,
-            * self.calculator_tool.calculator_tool_list])
-        
-        self.System_prompt = System_prompt
+class GraphBuilder():
+    def __init__(self,model_provider: str = "groq"):
         self.model_loader = ModelLoader(model_provider=model_provider)
         self.llm = self.model_loader.load_llm()
+        
+        self.tools = []
+        
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.calculator_tools = CalculatorTool()
+        self.currency_converter_tools = CurrencyCnverterTool()
+        
+        self.tools.extend([* self.weather_tools.weather_tool_list, 
+                           * self.place_search_tools.place_search_tool_list,
+                           * self.calculator_tools.calculator_tool_list,
+                           * self.currency_converter_tools.currency_converter_tool_list])
+        
         self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
-
+        
         self.graph = None
-
-    def agent_function(self,state:MessageState):
-        """Main Agent Function"""
+        
+        self.system_prompt = System_prompt
+    
+    
+    def agent_function(self,state: MessageState):
+        """Main agent function"""
         user_question = state["messages"]
-        input_question = [self.System_prompt] + user_question
+        input_question = [self.system_prompt] + user_question
         response = self.llm_with_tools.invoke(input_question)
-        return {"messages":[response]}
-
-
+        return {"messages": [response]}
     def build_graph(self):
-        graph_builder = StateGraph(MessageState) 
-
-        graph_builder.add_node("agent",self.agent_function)
-        graph_builder.add_node("tools",ToolNode(tools_condition))
-
+        graph_builder=StateGraph(MessageState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
         graph_builder.add_edge(START,"agent")
         graph_builder.add_conditional_edges("agent",tools_condition)
         graph_builder.add_edge("tools","agent")
         graph_builder.add_edge("agent",END)
-
         self.graph = graph_builder.compile()
         return self.graph
-
-
-
+        
     def __call__(self):
         return self.build_graph()
--- utils/model_loaders.py
+++ utils/model_loaders.py
@@ -1,43 +1,46 @@
-from langchain_groq import ChatGroq
-from typing_extensions import Literal , Optional , Any
-from pydantic import BaseModel , Field
 import os
+from dotenv import load_dotenv
+from typing import Literal, Optional, Any
+from pydantic import BaseModel, Field
 from utils.config_loaders import load_config
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 
 
-class ConfigLoader: 
+class ConfigLoader:
     def __init__(self):
-        print(f'Loaded config ....')
-        self.config = load_config() 
-
-    def __getitem__(self,key) : 
+        print(f"Loaded config.....")
+        self.config = load_config()
+    
+    def __getitem__(self, key):
         return self.config[key]
 
 class ModelLoader(BaseModel):
-    model_provider = Literal['groq'] = 'groq'
-    config : Optional[ConfigLoader] = Field(default=None,exclude=True)
+    model_provider: Literal["groq", "openai"] = "groq"
+    config: Optional[ConfigLoader] = Field(default=None, exclude=True)
 
-
-    def model_post_init(self,__context:Any) -> None : 
+    def model_post_init(self, __context: Any) -> None:
         self.config = ConfigLoader()
-
-
+    
     class Config:
         arbitrary_types_allowed = True
-
-
+    
     def load_llm(self):
         """
-        Docstring for load_llm
-        
-        :param self: Description
+        Load and return the LLM model.
         """
-        print("LLM Loading...")
-        print(f"loading model from provider: {self.model_provider}")
-        if self.model_provider == "groq" : 
-            print("Loading LLM from Groq .........")
+        print("LLM loading...")
+        print(f"Loading model from provider: {self.model_provider}")
+        if self.model_provider == "groq":
+            print("Loading LLM from Groq..............")
             groq_api_key = os.getenv("GROQ_API_KEY")
-            model_name = self.config['llm']['groq']['model_name']
-            llm = ChatGroq(model= model_name , api_key=groq_api_key)
-
+            model_name = self.config["llm"]["groq"]["model_name"]
+            llm=ChatGroq(model=model_name, api_key=groq_api_key)
+        elif self.model_provider == "openai":
+            print("Loading LLM from OpenAI..............")
+            openai_api_key = os.getenv("OPENAI_API_KEY")
+            model_name = self.config["llm"]["openai"]["model_name"]
+            llm = ChatOpenAI(model_name="o4-mini", api_key=openai_api_key)
+        
         return llm
+    
